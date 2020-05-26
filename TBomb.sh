9d0f567e1560443903f72a386a0d6e5fc35e4b46
shriharipoojary <58731547+shriharipoojary@users.noreply.github.com>
2020-05-27 00:30:02 +0530

    Newfile

--- TBomb.sh
+++ TBomb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m Spectrum Eduhome !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m TBomb \e[0m"
+echo -e "\e[1;32m SpectrumBomb \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.speedx ]];then
+if [[ -s update.Spectrum Eduhome]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By Spectrum Eduhome>update.Spectrum Eduhome
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -24,13 +24,12 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet SpectrumBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border Spectrum Eduhome
+echo -e "\e[4;34m This Bomber Was Created By Spectrum Eduhome\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: shriharipoojary9921@gmail.com \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -44,42 +43,42 @@ if [ $ch -eq 1 ];then
 clear
 echo -e "\e[1;32m"
 rm *.xxx >/dev/null 2>&1
-python3 bomber.py
+python3 spectrum.py
 rm *.xxx >/dev/null 2>&1
 exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
-python3 bomber.py call
+echo 'Call Bomb By Spectrum Eduhome'> call.xxx
+python3 spectrum.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
 elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
+git clone https://github.com/TheSpectrum Eduhome/SpectrumBomb
+if [[ -s SpectrumBomb/SpectrumBomb.sh ]];then
+cd SpectrumBomb
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm -rf  SpectrumBomb >> temp
+rm update.Spectrum Eduhome>> temp
 rm temp
-chmod +x TBomb.sh
+chmod +x SpectrumBomb.sh
 fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m SpectrumBomb Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
-./TBomb.sh
+./SpectrumBomb.sh
 exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
-figlet TBomb
+figlet SpectrumBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Spectrum Eduhome
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -89,14 +88,13 @@ echo "  [+] Protection List"
 echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
+echo -e "\e[1;33m      [*]  Spectrum Eduhome  \e[1;31m"
+echo "         [-] Mail At: shriharipoojary9921@gmail.com"
 echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
 echo "         [-] Ping At: http://wa.me/917600140353"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
 echo "         [-] Ping At: https://t.me/Rieltar"
 echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
-echo "         [-] Mail At: 0n1cOn3@gmx.ch"
 echo ""
 echo ""
 echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
@@ -106,8 +104,7 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[1;32m           Mail: shriharipoojary9921@gmail.com \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
 echo "Press Enter To Go Home"
 read a3
@@ -115,13 +112,12 @@ clear
 elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet SpectrumBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Spectrum Eduhome
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: shriharipoojary9921@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/Spectrum Eduhomegit \e[0m"
 echo " "
 exit 0
 else
